d97ca47196dd7785d8059b6762b1f60530907d06
Aleksandr <muzhev1990@mail.ru>
2023-05-25 00:31:13 +0500

    Edit Second Finish Task

--- src/test/java/ru/netology/AviaSoulsTest.java
+++ src/test/java/ru/netology/AviaSoulsTest.java
@@ -14,6 +14,7 @@ public class AviaSoulsTest {
     Ticket ticket3 = new Ticket("Москва", "Новосибирск", 40_000, 18_00, 22_00);
     Ticket ticket4 = new Ticket("Москва", "Новосибирск", 55_000, 3_00, 8_00);
     Ticket ticket5 = new Ticket("Москва", "Новосибирск", 15_000, 16_00, 21_00);
+    Ticket ticket6 = new Ticket("Москва", "Казань", 22_000, 6_00, 8_00);
 
 
 //    @BeforeEach
@@ -98,11 +99,15 @@ public class AviaSoulsTest {
 
     @Test
     public void testSearchCountTicket() {
+        aviaSouls.add(ticket1);
         aviaSouls.add(ticket2);
         aviaSouls.add(ticket3);
+        aviaSouls.add(ticket4);
+        aviaSouls.add(ticket5);
+        aviaSouls.add(ticket6);
 
-        Ticket[] expected = {ticket2, ticket3};
-        Ticket[] actual = aviaSouls.search("Москва", "Новосибирск");
+        Ticket[] expected = {ticket6};
+        Ticket[] actual = aviaSouls.search("Москва", "Казань");
 
         Assertions.assertArrayEquals(expected, actual);
     }
